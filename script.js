81ba9fbe82f75a069a0ece74a9bf599d19eede6e
Virgil <lunguvirgil3000@gmail.com>
2022-11-07 22:16:31 +0200

    Alpha v0.3.5: add event listener and console log the pokemon by the pokedex number

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const pokelist = document.getElementsByClassName('pokedex-pokelist');
-
+const pokenav = document.getElementById('pokenav')
 
 function fetchPokemon(idx) {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=905')
     .then(response => response.json())
-    .then(allPokemon => console.log(allPokemon['results'][idx]['name']))
+    .then(allPokemon => console.log(allPokemon['results'][idx-1]['name']))
 }
+
+pokenav.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        fetchPokemon(pokenav.value)
+    }
+})
 
 fetchPokemon(32)
